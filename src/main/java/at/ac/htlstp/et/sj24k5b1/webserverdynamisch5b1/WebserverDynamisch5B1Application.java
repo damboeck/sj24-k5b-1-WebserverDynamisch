86ee75efb244af4ee288a2da20cdc5b88f10e7da
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2024-11-04 15:27:25 +0100

    Initial commit

---
+++ src/main/java/at/ac/htlstp/et/sj24k5b1/webserverdynamisch5b1/WebserverDynamisch5B1Application.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.sj24k5b1.webserverdynamisch5b1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverDynamisch5B1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverDynamisch5B1Application.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/htlstp/et/sj24k5b1/webserverdynamisch5b1/WebserverDynamisch5B1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.sj24k5b1.webserverdynamisch5b1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverDynamisch5B1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
