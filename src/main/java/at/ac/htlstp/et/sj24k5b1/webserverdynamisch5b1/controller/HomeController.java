d09b8c2e7e37808e707262f3e35c4ca210906330
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2024-11-04 16:35:43 +0100

    form

--- src/main/java/at/ac/htlstp/et/sj24k5b1/webserverdynamisch5b1/controller/HomeController.java
+++ src/main/java/at/ac/htlstp/et/sj24k5b1/webserverdynamisch5b1/controller/HomeController.java
@@ -1,8 +1,12 @@
 package at.ac.htlstp.et.sj24k5b1.webserverdynamisch5b1.controller;
 
+import at.ac.htlstp.et.sj24k5b1.webserverdynamisch5b1.model.IndexFormDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
@@ -20,8 +24,19 @@ public class HomeController {
     @GetMapping("/abc")
     public String home(Model model) {
         model.addAttribute("msg", "Hallo Welt!");
+        model.addAttribute("data", new IndexFormDto("xx"));
         return "index";
     }
+
+    @PostMapping("/res")
+    public String formAnswer(Model model, @ModelAttribute IndexFormDto data) {
+        String feld = data.getFeld();
+        System.out.println(feld);
+        model.addAttribute("msg", "Antwort auf die Frage!");
+        model.addAttribute("data", data);
+        return "index";
+    }
+
 
 
 }
---
+++ src/main/java/at/ac/htlstp/et/sj24k5b1/webserverdynamisch5b1/model/IndexFormDto.java
@@ -0,0 +1,16 @@
+package at.ac.htlstp.et.sj24k5b1.webserverdynamisch5b1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IndexFormDto {
+
+    private String feld="";
+
+}
